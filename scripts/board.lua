bd167f25de69d4a6c06115cb705177fd6d60b6cb
Kristián Kunc <kristian.kunc@gmail.com>
2026-08-22 23:17:54 +0200

    eggs

---
+++ scripts/board.lua
@@ -0,0 +1,31 @@
+---@class board
+local M = {}
+
+---@type number
+M.SCALING = 100
+
+---@type string[]
+M.CORNER_KEYS = { "nw", "ne", "sw", "se" }
+
+---@type table<string, {x: number, y: number}>
+M.OFFSETS = {
+	nw = { x = -1, y = 1 },
+	ne = { x = 1, y = 1 },
+	sw = { x = -1, y = -1 },
+	se = { x = 1, y = -1 },
+}
+
+---Compute the world position of a square corner node.
+---@param origin vector3 the center of the square
+---@param corner string one of "nw", "ne", "sw", "se"
+---@return vector3
+function M.position(origin, corner)
+	local offset = M.OFFSETS[corner]
+	return vmath.vector3(
+		origin.x + offset.x * M.SCALING,
+		origin.y + offset.y * M.SCALING,
+		origin.z
+	)
+end
+
+return M
---
+++ scripts/eggs/egg.go
@@ -0,0 +1,16 @@
+components {
+  id: "egg"
+  component: "/scripts/eggs/egg.script"
+}
+embedded_components {
+  id: "egg-label"
+  type: "label"
+  data: "size {\n"
+  "  x: 64.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "text: \"o\"\n"
+  "font: \"/builtins/fonts/default.font\"\n"
+  "material: \"/builtins/fonts/label-df.material\"\n"
+  ""
+}
